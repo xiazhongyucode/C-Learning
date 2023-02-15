20eaf997425a5d97bfc784a3715d2d4cf831b437
Code <xiazhongyu2000@gmail.com>
2023-02-15 09:11:20 +0800

    commit

--- Online_Judge/lesson4_homework1/main.c
+++ Online_Judge/lesson4_homework1/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
+//判断一个数是否是对称数
 int main() {
-    int a=12321;
-    int b=0,backup_a;
-    backup_a=a;
+    int a,b=0,backup_a;
+    scanf("%d",&a);//读取一个整型数
+    backup_a=a;//把a的值备份一下
     while (a){
-        b=a%10;
+        b=b*10+a%10;//b把原有的值乘以10，然后再加余数
         a=a/10;
-        b=b+
     }
+//    printf("b = %d\n",b);//为了辅助自己理解
     //判断b和backup_a相等，就是对称数，如果不等，就不是对称数
     if(b==backup_a){
         printf("yes\n");
