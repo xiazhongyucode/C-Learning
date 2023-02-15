8af0255ebdcdf4bb5fbf6f391a2d2395e9f34d67
Code <xiazhongyu2000@gmail.com>
2023-02-15 09:18:22 +0800

    commit

--- Online_Judge/lesson4_homework1/main.c
+++ Online_Judge/lesson4_homework1/main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+
 //判断一个数是否是对称数
 int main() {
-    int a,b=0,backup_a;
-    scanf("%d",&a);//读取一个整型数
-    backup_a=a;//把a的值备份一下
-    while (a){
-        b=b*10+a%10;//b把原有的值乘以10，然后再加余数
-        a=a/10;
+    int a, b = 0, backup_a;
+    scanf("%d", &a);//读取一个整型数
+    backup_a = a;//把a的值备份一下
+    while (a) {
+        b = b * 10 + a % 10;//b把原有的值乘以10，然后再加余数
+        a = a / 10;
     }
 //    printf("b = %d\n",b);//为了辅助自己理解
     //判断b和backup_a相等，就是对称数，如果不等，就不是对称数
-    if(b==backup_a){
+    if (b == backup_a) {
         printf("yes\n");
-    } else{
+    } else {
         printf("no\n");
     }
     return 0;
---
+++ Online_Judge/lesson4_homework2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//求n的阶乘
+int main() {
+    int n;
+    scanf("%d", &n);
+    int i, result = 1;
+    for (i = 1; i <= n; i++) {
+        result *= i;
+    }
+    printf("%d\n", result);
+    return 0;
+}
