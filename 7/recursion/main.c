3718bf72e4d322f6d97168b4832667c80d00f13a
Code <xiazhongyu2000@gmail.com>
2023-02-17 10:59:11 +0800

    commit

--- 7/recursion/main.c
+++ 7/recursion/main.c
@@ -9,9 +9,18 @@ int f(int n) {
     return n * f(n - 1);//写公式
 }
 
+//上台阶，到第n个台阶，有多少种走法
+int step(int n) {
+    if (1 == n || 2 == n) {//当台阶是1个或者是2个时递归结束
+        return n;
+    }
+    return step(n - 1) + step(n - 2);
+}
+
 int main() {
     int n;
     scanf("%d", &n);
-    printf("f(%d) = %d\n", n, f(n));
+//    printf("f(%d) = %d\n", n, f(n));
+    printf("step(%d) = %d\n", n, step(n));
     return 0;
 }
