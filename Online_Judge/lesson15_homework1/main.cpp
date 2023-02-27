e52889a2e253494bfd08b9ed28ca257ad77d25db
Code <xiazhongyu2000@gmail.com>
2023-02-27 16:44:03 +0800

    commit

--- Online_Judge/lesson15_homework1/main.cpp
+++ Online_Judge/lesson15_homework1/main.cpp
@@ -45,20 +45,20 @@ void Create_BST(BiTree &T, KeyType *str, int len) {
 //中序遍历
 //往数组中存一个元素，下标就需要往后移动一次
 //int element_pos=0;
-void InOrder(BiTree T,int *arr) {
-    static int element_pos=0;//静态局部变量，只会初始化一次，和全局变量类似，只是只在函数内有效
+void InOrder(BiTree T, int *arr) {
+    static int element_pos = 0;//静态局部变量，只会初始化一次，和全局变量类似，只是只在函数内有效
     if (T != NULL) {
-        InOrder(T->lchild,arr);
+        InOrder(T->lchild, arr);
         printf("%3d", T->key);
         //打印的同时把数据存储到数组中
-        arr[element_pos]=T->key;//存入数组
-        element_pos=element_pos+1;//每存一次，就需要让下标往后移动一下
-        InOrder(T->rchild,arr);
+        arr[element_pos] = T->key;//存入数组
+        element_pos = element_pos + 1;//每存一次，就需要让下标往后移动一下
+        InOrder(T->rchild, arr);
     }
 }
 
 //实现二分查找
-int BinarySearch(int* arr, KeyType key,int len) {
+int BinarySearch(int *arr, KeyType key, int len) {
     int low = 0;
     int high = len - 1;
     int mid;
@@ -80,16 +80,16 @@ int main() {
     int arr[10];
     //读取标准输入的10个整型元素
     int i;
-    for(i=0;i<10;i++){
-        scanf("%d",&arr[i]);
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
     }
-    BiTree T=NULL;//给树初始化为NULL
-    Create_BST(T,arr,10);
+    BiTree T = NULL;//给树初始化为NULL
+    Create_BST(T, arr, 10);
     //中序遍历输出元素，同时存入数组arr
-    InOrder(T,arr);
+    InOrder(T, arr);
     printf("\n");
     int key_pos;
-    key_pos= BinarySearch(arr,21,10);
-    printf("%d\n",key_pos);
+    key_pos = BinarySearch(arr, 21, 10);
+    printf("%d\n", key_pos);
     return 0;
 }
