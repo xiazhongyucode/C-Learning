3861624490c5de3852faad7e82ab60d1e2d80688
Code <xiazhongyu2000@gmail.com>
2023-03-03 15:11:01 +0800

    commit

--- 17/heap_sort/main.cpp
+++ 17/heap_sort/main.cpp
@@ -33,6 +33,40 @@ void swap(ElemType &a, ElemType &b) {
     b = tmp;
 }
 
+//调整某个父亲节点，王道书上的
+void AdjustDown(ElemType A[], int k, int len) {
+    int i;
+    A[0] = A[k];
+    for (i = 2 * k; i <= len; i *= 2) {
+        if (i < len && A[i] < A[i + 1])//左子节点与右子节点比较大小
+            i++;
+        if (A[0] >= A[i])
+            break;
+        else {
+            A[k] = A[i];
+            k = i;
+        }
+    }
+    A[k] = A[0];
+}
+
+//用数组去表示树   类似层次建树  王道书上的
+void BuildMaxHeap(ElemType A[], int len) {
+    for (int i = len / 2; i > 0; i--) {
+        AdjustDown(A, i, len);
+    }
+}
+
+//王道书上的
+void HeapSort(ElemType A[], int len) {
+    int i;
+    BuildMaxHeap(A, len);//建立大顶堆
+    for (i = len; i > 1; i--) {
+        swap(A[i], A[1]);
+        AdjustDown(A, 1, i - 1);
+    }
+}
+
 //把某个子树调整为大根堆
 void AdjustDown1(ElemType A[], int k, int len) {
     int dad = k;//父亲的下标
@@ -57,10 +91,10 @@ void HeapSort1(ElemType A[], int len) {
     for (i = len / 2 - 1; i >= 0; i--) {
         AdjustDown1(A, i, len);
     }
-    swap(A[0],A[len-1]);//交换根部元素和最后一个元素
-    for(i=len-1;i>0;i--){//i代表的是剩余的无序数的数组的长度
-        AdjustDown1(A,0,i);//调整剩余元素变为大根堆
-        swap(A[0],A[i-1]);
+    swap(A[0], A[len - 1]);//交换根部元素和最后一个元素
+    for (i = len - 1; i > 1; i--) {//i代表的是剩余的无序数的数组的长度
+        AdjustDown1(A, 0, i);//调整剩余元素变为大根堆
+        swap(A[0], A[i - 1]);//交换根部元素和无序数的数组的最后一个元素
     }
 }
 
